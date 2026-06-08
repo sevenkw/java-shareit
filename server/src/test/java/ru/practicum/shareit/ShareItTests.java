fddf91929f007989f26d31e19cc971e35d7a3d1b
yaroslavanferov <yaroslavanferov@yandex.ru>
2026-06-08 23:36:38 +0300

    fix test on git

--- server/src/test/java/ru/practicum/shareit/ShareItTests.java
+++ server/src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class ShareItTests {
