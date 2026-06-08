78aea31e26857eec49026d6ae4b7cb8895f9b873
yaroslavanferov <yaroslavanferov@yandex.ru>
2026-06-09 00:30:41 +0300

    fix test on git

--- gateway/src/main/java/ru/practicum/shareit/item/ItemClient.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemClient.java
@@ -16,7 +16,7 @@ public class ItemClient extends BaseClient {
     private static final String API_PREFIX = "/items";
 
     @Autowired
-    public ItemClient(@Value("${backend.url}") String serverUrl, RestTemplateBuilder builder) {
+    public ItemClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
--- gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
@@ -15,7 +15,7 @@ public class UserClient extends BaseClient {
     private static final String API_PREFIX = "/users";
 
     @Autowired
-    public UserClient(@Value("${backend.url}") String serverUrl, RestTemplateBuilder builder) {
+    public UserClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
